260f2a1747a9d208e9bb25294fbc9fdbc05669d0
KJobanputra <kjobanputra@kenmarkitan.com>
2024-03-15 20:33:17 +0530

    Merge branch 'Krishna' into master

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Teams from './Components/Teams/Teams';
+import About from './Components/Teams/About';
 
 function App() {
   return (
     <div className="App">
-      <Teams/>
+      {/* <Teams/> */}
+      <About />
     </div>
   );
 }
