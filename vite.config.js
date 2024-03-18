4ccdf6c877a20009f7be4b4fc76981cc7f3b20a3
KJobanputra <kjobanputra@kenmarkitan.com>
2024-03-18 13:33:48 +0530

    minor Changes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    resolve({
-      extensions: ['.js', '.jsx'],
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx'],
-    }),
-  ],
+  plugins: [react()],
 })
