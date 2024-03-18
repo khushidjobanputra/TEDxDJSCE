c9c9bcf8efcab333c666b991c75e6d505511f5b8
KJobanputra <kjobanputra@kenmarkitan.com>
2024-03-18 12:44:18 +0530

    minor Changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    resolve({
+      extensions: ['.js', '.jsx'],
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx'],
+    }),
+  ],
 })
